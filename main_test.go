9bb66eafd5ed11129b326af78dfd168edf980b8c
Ornyx <cthulhu345@gmail.com>
2024-04-17 17:43:27 +1200

    Added a benchmark for just doing a regular draw

--- main_test.go
+++ main_test.go
@@ -10,7 +10,7 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-func BenchmarkMain(b *testing.B) {
+func BenchmarkBounce(b *testing.B) {
 	x1 := 1080
 	x2 := 1920
 	x3 := 0
@@ -50,4 +50,40 @@ func BenchmarkMain(b *testing.B) {
 		
 	}
 }
+
+func BenchmarkDraw(b *testing.B) {
+	x1 := 1080
+	x2 := 1920
+	x3 := 0
+	x4 := 0
+	x, y, w, h = &x4, &x3, &x2, &x1
+
+	fd, err := os.OpenFile("/dev/fb0", os.O_RDWR, 0644)
+	if err != nil {
+		b.Fatalf("Failed to open framebuffer: %v", err)
+	}
+	defer fd.Close()
+
+	mem, err := unix.Mmap(int(fd.Fd()), 0, *w**h*4, unix.PROT_WRITE, unix.MAP_SHARED)
+	if err != nil {
+		b.Fatalf("failed to mmap: %v", err)
+	}
+
+	file, err := os.Open("./sofa-cat.png")
+	if err != nil {
+		b.Fatalf("Unable to open image file: %v", err)
+	}
+
+	origMem = make([]byte, len(mem))
+	copy(origMem, mem)
+	defer file.Close()
+	
+	img, _, err := image.Decode(file)
+	if err != nil {
+		b.Fatalf("Unable to decode image: %v", err)
+	}
+
+	runFunc(img, &mem, *x, *y)
+}
+
 
